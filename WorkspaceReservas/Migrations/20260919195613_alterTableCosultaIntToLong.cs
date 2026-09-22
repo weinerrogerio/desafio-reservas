ebffb3e5a5b9e9dc8e37681ea0c8481c8ca2da39
weiner-rogerio <weiner-rogerio@DESKTOP-BB4CEQ5>
2026-09-21 21:39:45 -0300

    modificações em migrations

---
+++ WorkspaceReservas/Migrations/20260919195613_alterTableCosultaIntToLong.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WorkspaceReservas.Migrations
+{
+    /// <inheritdoc />
+    public partial class alterTableCosultaIntToLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "id_medico_fk",
+                table: "Consultas",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "id_medico_fk",
+                table: "Consultas",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
